5c3746bc27cc568412697ec955751a7882413280
Nikita Saleev <nvsaleev@gmail.com>
2025-03-23 01:59:30 -0500

    initialized the dashboard

--- pricing_engine/influx.py
+++ pricing_engine/influx.py
@@ -1,7 +1,30 @@
 import pandas as pd
+from os import environ as env
+
+from influxdb_client import InfluxDBClient, Point, WriteOptions
+from influxdb_client.client.write_api import SYNCHRONOUS
+
 
 def ingest_price_history(price_history: pd.DataFrame):
     pass
 
 def ingest_next_prices(price: pd.DataFrame):
     pass
+
+
+def get_influx_client() -> object:
+    return InfluxDBClient(
+        url=env.get("INFLUXDB_URL"), 
+        token=env.get("INFLUXDB_TOKEN"),
+        org=env.get("INFLUXDB_ORG"),
+        timeout=300_000
+    )
+
+
+def write_points(points: list, bucket: str):
+
+    client = get_influx_client()
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    write_api.write(bucket=bucket, org=env.get("INFLUXDB_ORG"), record=points)
+
+    client.close()
